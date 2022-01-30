624ffe58a718069e956105f7f5a2719a3fb0f536
rotemshaked <rotemshaked8@gmail.com>
2022-01-30 19:49:08 +0200

    toam finish the review

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -4,71 +4,75 @@ import "../assets/styles.css";
 import Calendar from "react-awesome-calendar";
 import axios from "axios";
 import { convertAllDates } from "../utils/convertDate";
+import { removeEmptySpaces } from "../utils/global";
+
+const events = {
+  1: {
+    id: 1,
+    color: "#fd3153",
+    from: "2022-02-02",
+    to: "2022-02-02",
+    title: "This is an event",
+  },
+  2: {
+    id: 2,
+    color: "#fd3153",
+    from: "2022-02-02",
+    to: "2022-02-02",
+    title: "This is an event",
+  },
+  3: {
+    id: 3,
+    color: "#fd3153",
+    from: "2022-02-02",
+    to: "2022-02-02",
+    title: "This is an event",
+  },
+};
 
 const CalendarComponent = () => {
   const navigate = useNavigate();
-  const [data, setData] = useState();
-  const [israel, setIsrael] = useState();
-  const [muslim, setMuslim] = useState();
-  const [christian, setChristian] = useState();
+  const [holidays, setHolidays] = useState([]);
+  const [israel, setIsrael] = useState([]);
+  const [muslim, setMuslim] = useState([]);
+  const [christian, setChristian] = useState([]);
+
   const navigateTo = (path) => {
     navigate(`/${path}`, { replace: true });
   };
-  const events = {
-    1: {
-      id: 1,
-      color: "#fd3153",
-      from: "2022-02-02",
-      to: "2022-02-02",
-      title: "This is an event",
-    },
-    2: {
-      id: 2,
-      color: "#fd3153",
-      from: "2022-02-02",
-      to: "2022-02-02",
-      title: "This is an event",
-    },
-    3: {
-      id: 3,
-      color: "#fd3153",
-      from: "2022-02-02",
-      to: "2022-02-02",
-      title: "This is an event",
-    },
-  };
 
   const handleClick = (id) => {
-    const path = events[id].title.replaceAll(" ", "");
+    const path = removeEmptySpaces(events[id].title);
     navigateTo("Holidays/" + path);
   };
 
+  const getDataByReligion = () => {
+    holidays.forEach((religion) => {
+      if (religion.region === "israel") setIsrael(religion.dates);
+
+      if (religion.region === "islam") setMuslim(religion.dates);
+
+      if (religion.region === "christian") setChristian(religion.dates);
+    });
+  };
+
   useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios.get("http://localhost:8080/get/holidays");
-      setData(data.data);
-      return data;
-    };
-    const getDataByReligion = async () => {
-      await getData();
-      data.forEach((religion) => {
-        if (religion.region === "israel") {
-          console.log(israel);
-          setIsrael(religion.dates);
-        }
-        if (religion.region === "islam") {
-          setMuslim(religion.dates);
-          console.log(muslim);
-        }
-        if (religion.region === "christian") {
-          setChristian(religion.dates);
-          console.log(christian);
-        }
-      });
-      convertAllDates(data.data);
-    };
-    getDataByReligion();
+    async function getHoliday() {
+      const { data: holidays } = await axios.get(
+        "http://localhost:8080/get/holidays"
+      );
+      const holidaysData = convertAllDates(holidays.data);
+      setHolidays(holidaysData);
+    }
+    getHoliday();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (!holidays.length) return;
+    getDataByReligion();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [holidays]);
 
   return (
     <div className="calendar-container">
