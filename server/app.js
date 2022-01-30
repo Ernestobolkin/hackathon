e4d7c4e8d6061e16edd1f4ab18b695db7fb6abee
Ernestobolkin <ernest1047@gmail.com>
2022-01-30 10:33:34 +0200

    another test

--- server/app.js
+++ server/app.js
@@ -3,16 +3,20 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 require("./mongoDB/mongoose");
+require("dotenv").config();
 
 const port = process.env.PORT || 8080;
-
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const publicPath = path.join(__dirname, './build');
+const publicPath = path.join(__dirname, "build");
 app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
