7c8bbe9130dc0235b82a8745a2ef9ef437cb3090
rotemshaked <rotemshaked8@gmail.com>
2022-01-31 11:57:50 +0200

    color button

--- client/src/components/colorKey.jsx
+++ client/src/components/colorKey.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+
 export const ColorKey = () => {
+  const [click, setClick] = useState(false);
+
   const onClickHandle = () => {
-    alert("Blue - Judaism \nRed - Christianity \nGreen - Muslim ");
+    setClick(!click);
   };
   return (
     <>
       <button onClick={onClickHandle} className="colorKey">
         Colors
       </button>
+      <span className={click && "colorKeyContiner"}>
+        {click && (
+          <span>
+            <span style={{ color: "blue" }}>Blue: Judaism &nbsp;|&nbsp;</span>
+            <span style={{ color: "red" }}>
+              Red: Christianity&nbsp;|&nbsp;{" "}
+            </span>
+            <span style={{ color: "green" }}>Green: Muslim</span>
+          </span>
+        )}
+      </span>
     </>
   );
 };
