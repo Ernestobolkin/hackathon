adfcc1c18407f9c20836af88f77c8012c9f1316b
Yitz <chalfari@gmail.com>
2022-01-31 11:38:24 +0200

    add logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../assets/styles.css";
-
+import logo from "../assets/logo.png";
 const Header = ({ back }) => {
   return (
     <div className="navbar">
+      <img src={logo} alt="diversity logo" />
       <Link onClick={back} className="link" to="/">
         Calendar
-      </Link>
-      <Link className="link" to="/Holidays">
-        Holidays
       </Link>
     </div>
   );
